a91ec70d37f1f0f55a59f43a62ecfefccce0de91
SahilJoshi5564 <74585821+SahilJoshi5564@users.noreply.github.com>
2026-07-22 18:19:12 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'compass-v18';
+const CACHE = 'compass-v22';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
